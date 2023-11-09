0b417f90ca1a01a45bfaf42a0dadca826e64f110
AdamS2002 <127023176+AdamS200@users.noreply.github.com>
2023-11-09 16:44:10 +0100

    Update Ray.h

--- RT/Source/Ray.h
+++ RT/Source/Ray.h
@@ -11,8 +11,15 @@ public:
     Ray(){}
     Ray(const point3& origin, const vec3& direction) : orig(origin), dir(direction){}
 
-    point3 origin() const {return orig;}
-    vec3 direction() const {return dir;}
+    point3 origin() const 
+    {
+        return orig;
+    }
+    vec3 direction() 
+    const
+   {
+    return dir;
+   }
     point3 at(double t)const
     {
         return orig +t*dir;
