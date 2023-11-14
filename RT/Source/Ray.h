2f98260096eedbca0913a8c3f658ac081ebb4b07
Puci123 <michaploc@wp.pl>
2023-11-14 20:14:40 +0100

    Linking paths fixed

--- RT/Source/Ray.h
+++ RT/Source/Ray.h
@@ -1,12 +1,10 @@
 #pragma once
 #include "MathUtility.h"
-#include <iostream>
-using namespace std;
 
 
 class Ray
 {
-public:
+	public:
 		Ray(mu::vec3 orgin, mu::vec3 dir) : m_Orgin(orgin), m_Dir(dir) {}
 		~Ray() {}
 
@@ -15,9 +13,9 @@ public:
 		inline mu::vec3 at(float dist)		const { return m_Orgin + m_Dir * dist; }	//Return positon after traveling certian distance 
 
 
-private:
+	private:
 		mu::vec3 m_Orgin;
 		mu::vec3 m_Dir;
 
 };
-#endif // Ray_h
+
